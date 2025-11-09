e820f3e948be85b7c5c921f2cd0b466ed2213513
Viktoriia <viktoriia.shcherbak@deliveryhero.com>
2025-11-09 16:02:45 +0100

    add test gifs

--- src/gif_player/display_gif_frames.c
+++ src/gif_player/display_gif_frames.c
@@ -40,7 +40,7 @@ static int s_preloaded_ready = 0;
 static int preload_all_gifs() {
     if (s_preloaded_ready) return 1;
     s_preloaded_count = 0;
-    DIR *dir = opendir(BASE_PATH);
+    DIR *dir = opendir('../../assets/gifs/');
     if (!dir) {
         perror("opendir(BASE_PATH)");
         return 0;
