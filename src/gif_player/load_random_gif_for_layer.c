0b3ae938e4bd35b9a232f91c01b8b79dde7f002e
Viktoriia <viktoriia.shcherbak@deliveryhero.com>
2025-11-22 19:58:15 +0100

    sort, clean, replace const with context

--- src/gif_player/load_random_gif_for_layer.c
+++ src/gif_player/load_random_gif_for_layer.c
@@ -1,5 +1,12 @@
 #include "../../main.h"
 
+
+static PreloadedGif s_preloaded[512];
+static int s_preloaded_count = 0;
+static int s_preloaded_ready = 0;
+const int max_loops = 10;
+const int min_loops = 5;
+
 int load_random_gif_for_layer(GifContext *layer)
 {
     if (!s_preloaded_ready || s_preloaded_count <= 0)
@@ -15,6 +22,6 @@ int load_random_gif_for_layer(GifContext *layer)
     layer->current_frame = 0;
     layer->current_path = sel->name;
     layer->loops_remaining = rand_range(min_loops, max_loops);
-    
+
     return 1;
 }
